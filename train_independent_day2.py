8a0d77dbc3eecf014d236c22e310184613e592e6
Jalolbek <j.otanazarov@newuu.uz>
2026-01-18 16:43:49 +0500

    3rdday

--- train_independent_day2.py
+++ train_independent_day2.py
@@ -55,6 +55,8 @@ def get_batch(split):
     return x, y
 
 class Head(nn.Module):
+
+
     def __init__(self, head_size, n_emb):
         super().__init__()
         self.query = nn.Linear(n_emb, head_size, bias=False)
@@ -62,6 +64,7 @@ class Head(nn.Module):
         self.value = nn.Linear(n_emb, head_size, bias=False)
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
     def forward(self, x):
         q = self.query(x) # (B, T, head_size)
         k = self.key(x) # (B, T, head_size)
@@ -75,11 +78,14 @@ class Head(nn.Module):
         return out
     
 class MultiHead(nn.Module):
+
+
     def __init__(self, n_emb):
         super().__init__()
         self.sa_heads = nn.ModuleList([Head(n_emb//n_head, n_emb) for _ in range(n_head)]) 
         self.proj = nn.Linear(n_emb, n_emb, bias=False)
         self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         x = torch.cat([h(x) for h in self.sa_heads], dim=-1)
         x = self.proj(x)
@@ -87,6 +93,8 @@ class MultiHead(nn.Module):
         return x
     
 class FeedForward(nn.Module):
+
+
     def __init__(self, n_emb):
         super().__init__()
         self.net = nn.Sequential(
@@ -95,22 +103,28 @@ class FeedForward(nn.Module):
             nn.Linear(4 * n_emb, n_emb, bias=False),
             nn.Dropout(dropout),
         )
+
     def forward(self, x):
         return self.net(x)
 
 class Block(nn.Module):
+
+
     def __init__(self, n_emb):
         super().__init__()
         self.sa = MultiHead(n_emb)
         self.ffwd = FeedForward(n_emb)
         self.ln1 = nn.LayerNorm(n_emb)
-        self.ln2 = nn.LayerNorm(n_emb)  
+        self.ln2 = nn.LayerNorm(n_emb) 
+         
     def forward(self, x):
         x = x + self.sa(self.ln1(x))
         x = x + self.ffwd(self.ln2(x))
         return x
 
 class BigramLanguageModel(nn.Module):
+
+
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_emb)
@@ -118,6 +132,16 @@ class BigramLanguageModel(nn.Module):
         self.blocks = nn.Sequential(*[Block(n_emb) for _ in range(n_layer)])
         self.ln = nn.LayerNorm(n_emb)
         self.lm_head = nn.Linear(n_emb, vocab_size, bias=False)
+        self.apply(self._init_weights)
+        
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
     def forward(self, idx, targets=None):
         B, T = idx.shape
         tok_emb = self.token_embedding_table(idx)
@@ -134,6 +158,7 @@ class BigramLanguageModel(nn.Module):
             targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
         return logits, loss
+    
     def generate(self, idx, max_tokens):
         for _ in range(max_tokens):
             idx_crop = idx[:, -block_size:]
@@ -143,6 +168,7 @@ class BigramLanguageModel(nn.Module):
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat([idx, idx_next], dim=1)
         return idx
+    
 model = BigramLanguageModel()
 m = model.to(device)
 
